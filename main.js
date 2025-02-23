d42d79602c71251ea614ede9e0d56e9f181603f4
Sirkoku <faucrivelli@gmail.com>
2025-02-22 23:50:37 -0300

    error en alerta

--- main.js
+++ main.js
@@ -1,29 +1,4 @@
-// Palabra reservada, nombre, dato a almacenar
-/* let nombre = "martin"
 
-
-const pi= 3.14 
-
-"45" String
-45 Number */
-
-/* let numero1 = 1
-let numero2 = parseFloat ("2") // el parse convierte cualquier string (que se pueda) a un entero
-
-let resultado = numero1+numero2
-
-alert (resultado)
-
-console.log (resultado)
-*/
-/*
-let nombre = prompt("ingrese su nombre")
-let apellido = prompt ("ingrese su apellido")
-
-alert ("bienvenido al sistema " + nombre + " " + apellido)*/
-// $valor // 
-/*
-alert(`bienvenido al sistema ${nombre} , me entere que tu apellido es ${apellido}`) */
 
 function login(){
     let intentos = 1 
@@ -40,7 +15,8 @@ function login(){
         if(usuario === "milton salazar" && intentos <=3){
             alert (`Bienvenido tutor ${usuario}`)
             identificar = false
-        }else{
+        }
+        else{
             alert ("no reconozco el usuario")
             intentos ++
             if (intentos > 3){
