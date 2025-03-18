a7775cd3696ca533525951e57a5c1e3442244f8b
Sirkoku <faucrivelli@gmail.com>
2025-03-17 22:18:43 -0300

    segunda preentrega

--- main.js
+++ main.js
@@ -8,18 +8,14 @@ function login(){
         if (usuario === null){
             break
         }
-        if(usuario === "fausto" && intentos <=3){
+        if ((usuario === "fausto" || usuario === "milton") && intentos < 3){
             alert(`bienvenido al sistema Sr. ${usuario}`)
             identificar = false
-            calcularPrecioFinal()
-        } else if(usuario === "milton" && intentos <=3){
-            alert (`Bienvenido tutor ${usuario}`)
-            identificar = false
-            calcularPrecioFinal()
-        } else {
+            
+        }else {
             alert ("no reconozco el usuario")
             intentos ++
-            if (intentos > 3){
+            if (intentos >=3){
                 alert ("usted ha superado los intentos")
                 break
             }
@@ -27,21 +23,41 @@ function login(){
         } while (identificar) 
     }
 
-const iva = 1.21
-
-function calcularIva(importe){
-    if(parseFloat (importe)){
-        let resultado = importe * iva
-        alert("el importe mas iva es: "+ resultado)
-    }
-}
-
-
-function calcularPrecioFinal (){
-    let precioDelProducto = prompt("ingresa el importe total de la compra")
-    calcularIva(precioDelProducto)
-}
-
 login();
 
+const Bebida = function (nombre,precio,stock){
+    this.nombre = nombre
+    this.precio = precio
+    this.stock  = stock
+}
 
+let bebidas1 = new Bebida("vino cosecha tardia",4000, 20)
+let bebidas2 = new Bebida("vino cordero con piel de lobo",4000,20)
+let bebidas3 = new Bebida("vino trumpeter",4500,15)
+let bebidas4 = new Bebida("vino oveja negra",4800,25)
+let bebidas5 = new Bebida("fernet branca 1lt",14000,50)
+let bebidas6 = new Bebida("Gin bombay",16000,40)
+
+const lista = [bebidas1 ,bebidas2 ,bebidas3 ,bebidas4 ,bebidas5 , bebidas6]
+
+
+
+function filtrodeproductos(){
+    let productoUs = prompt("ingrese el producto que esta buscando").trim().toUpperCase()
+    console.log("producto a buscar", productoUs)
+
+
+    let resultado = lista.filter((x) => {
+        console.log(x.nombre);  // Verifica qué está tomando como nombre en cada iteración.
+        return x.nombre && x.nombre.toUpperCase().includes(productoUs);
+    });
+     // Mostrar resultado
+    if (resultado.length > 0) {
+        console.table(resultado);  // Muestra el resultado en una tabla.
+    } else {
+        alert("No hay coincidencias en el catálogo.");
+    }
+
+}
+filtrodeproductos();
+console.log(lista)
