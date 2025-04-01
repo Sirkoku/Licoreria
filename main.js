8e3338027d7f3fe020e1c924c4ca06292b28b91d
Sirkoku <faucrivelli@gmail.com>
2025-04-01 15:56:43 -0300

    tercera preentrega

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+/*
 function login(){
     let intentos = 1 
     let identificar = true 
@@ -23,40 +23,10 @@ function login(){
         } while (identificar) 
     }
 
-login();
-
-const Bebida = function (nombre,precio,stock){
-    this.nombre = nombre
-    this.precio = precio
-    this.stock  = stock
-}
-
-let bebidas1 = new Bebida("vino cosecha tardia",4000,20)
-let bebidas2 = new Bebida("vino cordero con piel de lobo",4000,20)
-let bebidas3 = new Bebida("vino trumpeter",4500,15)
-let bebidas4 = new Bebida("vino oveja negra",4800,25)
-let bebidas5 = new Bebida("fernet branca 1lt",14000,50)
-let bebidas6 = new Bebida("gin bombay",16000,40)
-
-const lista = [bebidas1 ,bebidas2 ,bebidas3 ,bebidas4 ,bebidas5 , bebidas6]
+login(); */
 
 
-
-function filtrarproductos(){
-    let productoUs = prompt("ingrese el producto que esta buscando").trim().toUpperCase()
-    console.log("producto a buscar", productoUs)
-    
-    let resultado = lista.filter((x) => x.nombre.toUpperCase().includes(productoUs)
-    );
-     // Mostrar resultado
-    if (resultado.length > 0) {
-        console.table(resultado);  // Muestra el resultado en una tabla.
-    } else {
-        alert("No hay coincidencias en el catálogo.");
-    }
-
-}
-
+/*
 function agregarprod(){
     let nombre = prompt("ingrese la bebida que desea agregar").trim()
     let precio = parseFloat(prompt("ingrese el valor del producto"))
@@ -74,20 +44,14 @@ function agregarprod(){
 
 }
 filtrarproductos();
-agregarprod();
+agregarprod();  */
+
 /*
 let bienvenida = document.getElementById("bienvenida")
-console.log(bienvenida.innerHTML)
-
-let titulo = document.getElementsByClassName("titulo")
-console.log(titulo[2].innerHTML)
-
-let texto = document.getElementsByTagName("p")
-console.log(texto.innerHTML)  */
-
-let bienvenida = document.getElementById("bienvenida")
+*/
 
 /*bienvenida.innerText =  "modifica solo el texto" */
+/*
 
 bienvenida.innerHTML = "<h1>Bienvenida modificada desde js</h1>"
 
@@ -96,6 +60,92 @@ let referencias = document.createElement("p")
 referencias.innerHTML = "<p>referencias inyectadas desde JS </p>"
 
 document.body.appendChild(referencias)
+*/
+const Bebida = function (nombre,precio,stock){
+    this.nombre = nombre
+    this.precio = precio
+    this.stock  = stock
+}
+
+let bebidas1 = new Bebida("vino cosecha tardia",4000,20)
+let bebidas2 = new Bebida("vino cordero con piel de lobo",4000,20)
+let bebidas3 = new Bebida("vino trumpeter",4500,15)
+let bebidas4 = new Bebida("vino oveja negra",4800,25)
+let bebidas5 = new Bebida("fernet branca 1lt",14000,50)
+let bebidas6 = new Bebida("gin bombay",16000,40)
+
+let lista = [bebidas1 ,bebidas2 ,bebidas3 ,bebidas4 ,bebidas5 , bebidas6]
+
+if(localStorage.getItem("bebidas")){
+    lista= JSON.parse(localStorage.getItem("bebidas"))
+}
+
+function filtrarproductos(){
+    let productoUs = prompt("ingrese el producto que esta buscando").trim().toUpperCase()
+    console.log("producto a buscar", productoUs)
+    
+    let resultado = lista.filter((x) => x.nombre.toUpperCase().includes(productoUs)
+    );
+     // Mostrar resultado
+    if (resultado.length > 0) {
+        console.table(resultado);  // Muestra el resultado en una tabla.
+    } else {
+        Swal.fire("No hay coincidencias en el catálogo.");
+    } 
+
+}
+
+function agregarproducto(){
+
+    Swal.fire({
+
+        title: "agregar bebida",
+        html: `<label>Nombre:</label> <input id="nombre-input" class="swal2-input" type= "text" autofocus> 
+            <label>Precio:</label> <input id="precio-input" class="swal2-input" type= "number" step="0.01">
+            <label>Stock:</label> <input id="stock-input" class="swal2-input" type= "number" step="1">`,
+            showCancelButton: true,
+            confirmButtonText: "agregar",
+            cancelButtonText: "cancelar",
+    }).then((result)=>{
+        if(result.isConfirmed){
+            let nombre= document.getElementById("nombre-input").value.trim()
+            let precio= parseFloat(document.getElementById("precio-input").value)
+            let stock =parseInt(document.getElementById("stock-input").value) 
+            
+            if(isNaN(precio) || isNaN(stock) || nombre === ""){
+                Swal.fire({
+                    icon: "error",
+                    title: "error",
+                    text: "por favor ingrese todos los datos"
+                }); return
+            }
+            
+            let bebida = new Bebida(nombre,precio,stock)
+
+            if(lista.some((b)=> b.nombre === bebida.nombre)){
+                Swal.fire({
+                icon:"warning",
+                title:"advertencia",
+                text:"el producto ya existe en la lista"
+                })
+                return
+            }
+            lista.push(bebida);
+
+            Swal.fire({
+                icon:"success",
+                title:"bebida agregada",
+                text:`el producto ${bebida.nombre} se agrego al carrito `,
+                timer: 2500 //tiempo en milisegundos//,
+            });
+            console.table(lista);
+            }
+            })
+            }
 
 
+            let boton= document.getElementById("boton")
+            boton.addEventListener("click",agregarproducto)
 
+filtrarproductos();
+agregarproducto();
