eed1c4cb602e24b136c4dc6115a0dc11a885fd9a
Sirkoku <faucrivelli@gmail.com>
2025-04-28 22:59:08 -0300

    agregue tempo al inicio

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 const textoIntro = "Inspirada en Baco, el dios del vino y la celebración, nuestra empresa busca ofrecer calidad para los amantes del buen beber.";
 let i = 0;
-const speed = 30;
+const speed = 20;
 const textoElemento = document.getElementById("introText");
 
 function escribirTexto() {
@@ -10,6 +10,10 @@ function escribirTexto() {
     i++;
     setTimeout(escribirTexto, speed);
   }
+}else{
+  setTimeout(() =>{
+    window.location.href= "index.html";
+  },1000);
 }
 
 window.addEventListener('DOMContentLoaded',() =>{
